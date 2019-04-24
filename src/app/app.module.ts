32d07b5355ef31bae30607f170c9b9b382446690
Dion Roloff <dion.roloff@gmail.com>
2019-04-24 17:22:10 -0500

    this will add links in the navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { HomeComponent } from './home/home.component';
-import { DatabaseComponent } from './database/database.component';
-import { FormsComponent } from './forms/forms.component';
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { HomeComponent } from "./home/home.component";
+import { DatabaseComponent } from "./database/database.component";
+import { FormsComponent } from "./forms/forms.component";
+import { routing } from "./app.routing";
 
 @NgModule({
   declarations: [
@@ -16,11 +18,8 @@ import { FormsComponent } from './forms/forms.component';
     DatabaseComponent,
     FormsComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, FormsModule, HttpModule, routing],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
