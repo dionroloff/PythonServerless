38f6205454d1503701e71f39a7a9e166c3e7081e
Dion Roloff <dion.roloff@gmail.com>
2019-04-24 17:11:15 -0500

    this will add database, forms, home, and navbar components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { DatabaseComponent } from './database/database.component';
+import { FormsComponent } from './forms/forms.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    DatabaseComponent,
+    FormsComponent
   ],
   imports: [
     BrowserModule,
